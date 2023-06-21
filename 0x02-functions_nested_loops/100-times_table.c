dc24f91de833eef5ab176078ab1500afff49861b
joburtab <abraham.sh124@gmail.com>
2023-06-21 14:38:33 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,30 +1,20 @@
-/*
- * File: 100-times_table.c
- * Auth: Michael Ndungu Kamotho
- */
 
 #include "main.h"
 
 /**
- * 100-times_table - function that prints the n times table, starting with 0
- * @c: The character to be checked.
+ * main - check the code.
  *
- * Return: 1 if character is lowercase, 0 otherwise.
+ * Return: Always 0.
  */
-
-void times_table(int n){
-	int number = 0;
-	int by = 0;
-	int result, count;
-	for(count = 0;count < n; count++){
-	
-	   for(count = 0; count < 10; count++){
-	      result = number * by;
-	      by++;
-	      _putchar(result);
-	   }
-	number++;
-	_putchar('\n')
-	}
+int main(void)
+{
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
 }
 
