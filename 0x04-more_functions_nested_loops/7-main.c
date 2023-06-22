e61879013034ec95b5e081fbd81d044d06ffff5c
joburtab <abraham.sh124@gmail.com>
2023-06-22 12:51:59 -0700

    7-main.c

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -1,12 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
-void print_diagonal(int n);
-
 int main(void)
 {
     print_diagonal(0);
