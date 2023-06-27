7c52a2e05399f9a1eb54a1afbee973c95f0dadc9
joburtab <abraham.sh124@gmail.com>
2023-06-27 15:49:38 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,23 @@
-some
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    srand(time(0));
+
+    char password[7];
+    int i;
+
+    for (i = 0; i < 7; i++)
+    {
+        password[i] = rand() % 94 + 33; // Generate a random ASCII character between 33 and 126
+    }
+
+    password[i] = '\0'; // Null-terminate the password string
+
+    printf("%s", password);
+
+    return 0;
+}
+
